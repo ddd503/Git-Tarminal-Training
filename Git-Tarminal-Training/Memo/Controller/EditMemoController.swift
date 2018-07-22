b5b2862407ce7a81cc809f24c403b06835b20c90
ddd503 <skbsporta7@gmail.com>
2018-07-22 22:53:29 +0900

    コミット漏れ

--- Git-Tarminal-Training/Memo/Controller/EditMemoController.swift
+++ Git-Tarminal-Training/Memo/Controller/EditMemoController.swift
@@ -58,7 +58,7 @@ extension EditMemoController: MemoDataDaoDelegate {
         guard
             let navigationController = self.navigationController,
             let memoListController = navigationController.viewControllers[navigationController.viewControllers.count - 2] as? MemoListController else {
-            // 普通に戻る
+            // 普通に戻ると
             self.navigationController?.popViewController(animated: true)
             return
         }
